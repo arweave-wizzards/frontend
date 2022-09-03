8528f774ebf4f6d1df26dc135c44668991f51a77
Maksym Malicki <maksym.malicki.2115@gmail.com>
2022-09-03 12:10:22 +0100

    feat: add project boilerplate, add module-per-component, add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { PostsListComponent } from './reusable-modules/posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsListComponent,
   ],
   imports: [
     BrowserModule,
