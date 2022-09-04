1ec96f120fb74a361645623de5a967c994e25a42
Maksym Malicki <maksym.malicki.2115@gmail.com>
2022-09-04 01:51:29 +0100

    feat: added creating post logic

---
+++ src/app/routing-modules/write-post/post-form-data.interface.ts
@@ -0,0 +1,5 @@
+export interface PostFormData {
+  title: string;
+  content: string;
+  category: "Beginner" | "Intermediate" | "Advanced";
+}
--- src/app/routing-modules/write-post/write-post.component.ts
+++ src/app/routing-modules/write-post/write-post.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {PostFormData} from "./post-form-data.interface";
 
 @Component({
   templateUrl: './write-post.component.html',
   styleUrls: ['./write-post.component.scss']
 })
-export class WritePostComponent implements OnInit {
+export class WritePostComponent{
 
-  constructor() { }
+  @ViewChild('form') public readonly loginForm: NgForm;
 
-  ngOnInit(): void {
+  constructor(
+  ) {
+  }
+
+  public get getFormData(): PostFormData {
+    return this.loginForm.value;
+  }
+
+  public onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(this.getFormData);
+    this.loginForm.resetForm();
+    const id: number = 1;
+    window.open(`/single-post/${id}`, '_blank');
   }
 
 }
