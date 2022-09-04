7502c993e6698990bc61843106d8d3a071850c69
dknuszkiewicz@gmail.com <dknuszkiewicz@gmail.com>
2022-09-04 09:24:08 +0200

    refactor: some cleanings

--- src/app/routing-modules/interval-posts/interval-posts.component.ts
+++ src/app/routing-modules/interval-posts/interval-posts.component.ts
@@ -1,10 +1,7 @@
-import { AfterViewInit, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { BlogContractService } from 'src/app/shared-services/blog-contract.service';
 import { Post } from "../../shared-interfaces/post.interface";
-import userWallet from '../../../../../dominik-wallet.json';
-import { async } from '@angular/core/testing';
-import { BehaviorSubject, delay, Observable, of, Subscribable, Subscription } from 'rxjs';
-import { WalletConnectService } from 'src/app/shared-services/wallet-connect.service';
+
 
 
 @Component({
